27a6370e78862800522ebe86ed149ee9db320077
The_Valour <108991640+TheValour@users.noreply.github.com>
2023-02-07 20:43:58 +0530

    Update firebaseconfig.js

--- main/src/firebaseconfig.js
+++ main/src/firebaseconfig.js
@@ -3,12 +3,7 @@ import {getFirestore} from "@firebase/firestore"
 import {getStorage} from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDKZ9SfS-b7J2UsZGmDnrtGCUkfSQCP020",
-  authDomain: "mission-a-f722e.firebaseapp.com",
-  projectId: "mission-a-f722e",
-  storageBucket: "mission-a-f722e.appspot.com",
-  messagingSenderId: "690956525652",
-  appId: "1:690956525652:web:b995fdd6020ed59d0ecdc1"
+  // firebase configuration
 };
 
 const app = initializeApp(firebaseConfig);
